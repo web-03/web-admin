c52ca104722e996fe27790da1076d4b6c05f6507
ndtra <44887442+ndtra@users.noreply.github.com>
2019-04-19 10:51:30 +0700

    order

--- routes/orders.js
+++ routes/orders.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+var data = [
+  { id: '1', productName: 'Giày', categoryName: 'Giày', quantity: '10', unitPrice : '100000', totalAmount:'1000000', status: '2' },
+  { id: '2', productName: 'Dép', categoryName: 'Dép', quantity: '15', unitPrice : '20000', totalAmount:'300000', status: '1' },
+  { id: '3', productName: 'Vòng tay', categoryName: 'Trang sức', quantity: '5', unitPrice : '50000', totalAmount:'250000', status: '0' },
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('order/index',{title:'Danh sách đơn hàng'})
+  res.render('order/index',{orders :  data})
 });
 
 router.get('/completed', function(req, res, next) {
