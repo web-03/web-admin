9525c1423ba7627c421ad304153173cad10cad1c
ndtra <44887442+ndtra@users.noreply.github.com>
2019-05-27 20:59:55 +0700

    không được xóa tài khoản hiện hành

--- controller/employees.js
+++ controller/employees.js
@@ -8,42 +8,46 @@ var employeesAll = [];
 
 /* GET home page. */
 router.list = (req, res, next) => {
-  
+
   con.query('select * from employees', function (err, rows, fields) {
     if (err) throw err
-    employeesAll=[];
+    employeesAll = [];
     rows.forEach(element => {
       var x = new employee(element.id, element.name, element.account, element.phoneNumber, element.place, element.status);
       employeesAll.push(x);
     });
-    res.render('employee/index',{employees : employeesAll,user: req.user});
+    res.render('employee/index', { employees: employeesAll, user: req.user });
   });
-  
+
 };
 
 router.profile = (req, res, next) => {
-  res.render('employee/profile',{user: req.user});
+  res.render('employee/profile', { user: req.user });
 };
 
 router.changeStatus = (req, res, next) => {
-  
-  let id= req.params.id;
-  let x, r;
-  let sqlselect = "select * from employees where id="+id;
-  con.query(sqlselect, function(err, results, fields){
-    x = results[0].status;
-    if(x == 1){
-      r = 0;
-    }
-    else{
-      r = 1;
-    }
-    let sql = 'UPDATE employees SET status='+r+' WHERE id='+id;
-    con.query(sql);
-    res.redirect('/nhan-vien');
-    
-  });
-  
+  let employeeIdCurrent = req.user.id;
+  let id = req.params.id;
+  if (employeeIdCurrent == id) {
+    res.render('employee/change_status_fail', { employees: employeesAll, user: req.user });
+  }
+  else {
+    let x, r;
+    let sqlselect = "select * from employees where id=" + id;
+    con.query(sqlselect, function (err, results, fields) {
+      x = results[0].status;
+      if (x == 1) {
+        r = 0;
+      }
+      else {
+        r = 1;
+      }
+      let sql = 'UPDATE employees SET status=' + r + ' WHERE id=' + id;
+      con.query(sql);
+      res.redirect('/nhan-vien');
+
+    });
+  }
 };
 
 
@@ -51,8 +55,8 @@ router.create = (req, res, next) => {
   let name = req.body.name;
   let status = 1;
   let id = req.body.id;
-  if(id==""){
-    id=0;
+  if (id == "") {
+    id = 0;
   }
   let phoneNumber = req.body.phoneNumber;
   let place = req.body.place;
@@ -60,64 +64,64 @@ router.create = (req, res, next) => {
   console.log(id);
   console.log(name);
   console.log(password);
-  if(id == 0){
-    let sql='INSERT INTO employees(name,account,password,phoneNumber,place, status) VALUES ( "'+name+'" , '+ '"abc"' +' ,"'+password+ '", '+' "'+phoneNumber+'",'+'"'+place+'",'+status+')';
+  if (id == 0) {
+    let sql = 'INSERT INTO employees(name,account,password,phoneNumber,place, status) VALUES ( "' + name + '" , ' + '"abc"' + ' ,"' + password + '", ' + ' "' + phoneNumber + '",' + '"' + place + '",' + status + ')';
     console.log(sql);
     con.query(sql);
-    let sqlFind = 'select * from employees where account = '+'"abc"';
+    let sqlFind = 'select * from employees where account = ' + '"abc"';
     console.log(sqlFind);
-    con.query(sqlFind, function(err, results, fields){
+    con.query(sqlFind, function (err, results, fields) {
       x = results[0].id;
-      let newAccount = 'NV'+x;
+      let newAccount = 'NV' + x;
       console.log(newAccount);
-      let sqlUpdate = 'UPDATE employees SET account="'+newAccount+'" WHERE id='+x;
+      let sqlUpdate = 'UPDATE employees SET account="' + newAccount + '" WHERE id=' + x;
       con.query(sqlUpdate);
       res.redirect('/nhan-vien');
     });
-    
+
   }
-  else{
-    if(password == ""){
-      let sql = 'UPDATE employees SET name="'+name+'",phoneNumber="'+phoneNumber+'",place="'+place+'", status='+status+' WHERE id='+id;
+  else {
+    if (password == "") {
+      let sql = 'UPDATE employees SET name="' + name + '",phoneNumber="' + phoneNumber + '",place="' + place + '", status=' + status + ' WHERE id=' + id;
       con.query(sql);
       res.redirect('/nhan-vien');
     }
-    else{
-      let sql = 'UPDATE employees SET name="'+name+'",password="'+password+'",phoneNumber="'+phoneNumber+'",place="'+place+'", status='+status+' WHERE id='+id;
+    else {
+      let sql = 'UPDATE employees SET name="' + name + '",password="' + password + '",phoneNumber="' + phoneNumber + '",place="' + place + '", status=' + status + ' WHERE id=' + id;
       con.query(sql);
       res.redirect('/nhan-vien');
     }
   }
-  
-  
+
+
 };
 router.changePassword = (req, res, next) => {
-  
-    res.render('employee/changePassword',{msg:"" ,user: req.user});
-    
+
+  res.render('employee/changePassword', { msg: "", user: req.user });
+
 };
 router.saveNewPassword = (req, res, next) => {
   let account = req.user.account;
   // let oldPassword = bcrypt.hashSync(req.body.oldPassword, null, null);
   let oldPassword = req.body.oldPassword;
   let password = bcrypt.hashSync(req.body.newPassword, null, null);
-  console.log("oldpass: "+oldPassword);
+  console.log("oldpass: " + oldPassword);
   let x, r;
-  let sqlselect = 'select * from employees where account="'+account+'"';
-  con.query(sqlselect, function(err, results, fields){
+  let sqlselect = 'select * from employees where account="' + account + '"';
+  con.query(sqlselect, function (err, results, fields) {
     x = results[0].password;
     console.log(x);
-    if(bcrypt.compareSync(oldPassword, x)){
-      let sql = 'UPDATE employees SET password="'+password+'" WHERE account="'+account+'"';
-      con.query(sql,function(err,results,fields){});
-      
-      res.render('employee/changePassword',{user: req.user,msg: "Đổi mật khẩu thành công!"});
+    if (bcrypt.compareSync(oldPassword, x)) {
+      let sql = 'UPDATE employees SET password="' + password + '" WHERE account="' + account + '"';
+      con.query(sql, function (err, results, fields) { });
+
+      res.render('employee/changePassword', { user: req.user, msg: "Đổi mật khẩu thành công!" });
     }
-    else{
-      res.render('employee/changePassword',{user: req.user,msg: "Mật khẩu cũ không chính xác!"});
+    else {
+      res.render('employee/changePassword', { user: req.user, msg: "Mật khẩu cũ không chính xác!" });
     }
   });
-  
+
 };
 
 module.exports = router;
